b26e1ddc95e8dcbde153d721c257658a07e810b5
Arifabegum1104 <144448274+Arifabegum1104@users.noreply.github.com>
2023-09-21 16:37:54 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,37 +1,30 @@
-class BankAccount:
+class Student:
 
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      print("Deposited ₹{}. New balance: ₹{}".format(amount,
-                                                     self.__account_balance))
-    else:
-      print("The deposit amount is invalid")
-
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      print("withdrew ₹{}. New balance: ₹{}".format(amount,
-                                                    self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance.")
-
-  def display_balance(self):
-    print("Account balance for {} (Account #{}): ₹{}".format(
-        self.__account_holder_name, self.__account_number,
-        self.__account_balance))
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
 
-account = BankAccount(account_number="612438953",
-                      account_holder_name="harshini",
-                      initial_balance=5000.0)
+def sort_students(student_list):
 
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.display_balance()
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+
+  return sorted_students
+
+
+students = [
+    Student("priya", "A123", 7.8),
+    Student("sri", "A124", 8.9),
+    Student("devi", "A125", 9.1),
+    Student("mahi", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+  print("Name: {}, Roll number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
--- main.py
+++ main.py
@@ -1,28 +1,15 @@
-# Define the base class player
-class player:
+def linearsearchproduct(ProductList, targetproduct):
+  indices = []
 
-  def play(self):
-    print("The player is playing cricket.")
+  for index, product in enumerate(ProductList):
+    if product == targetproduct:
+      indices.append(index)
 
-
-# Define the derived class Batsman
-class Batsman(player):
-
-  def play(self):
-    print("The batsman is batting.")
+  return indices
 
 
-# Define the derived class Bowler
-class Bowler(player):
-
-  def play(self):
-    print("The bowler is bowling.")
-
-
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
-
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+Products = ["Shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearsearchproduct(Products, target)
+print(result)
